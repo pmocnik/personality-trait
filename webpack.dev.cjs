d1610328ab2c4e9877ee2f8ec0d70e44c6073861
Peter Mocnik <peter.mocnik@teletech.si>
2022-06-28 16:01:48 +0200

    Remove babel-register, use .cjs extension instead.

--- webpack.dev.cjs
+++ webpack.dev.cjs
@@ -1,6 +1,6 @@
 const { merge } = require('webpack-merge');
 const path = require('path');
-const common = require('./webpack.common.babel.js');
+const common = require('./webpack.common.cjs');
 
 
 module.exports = merge(common, {
